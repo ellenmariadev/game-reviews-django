1d0ff4f0315875526239b843376793842473aa13
ellenmariadev <ellenmariadev@gmail.com>
2022-11-16 17:13:26 -0300

    fix: review rating model

--- games/models.py
+++ games/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import Avg
 
 # Create your models here.
 
@@ -18,27 +17,19 @@ class Games(models.Model):
     publisher = models.CharField(max_length=100, null=True, blank=True)
     image = models.ImageField(upload_to='games/covers/%Y/%m/%d/')
     created_at = models.DateTimeField(auto_now_add=True)
-    genre = models.ManyToManyField(Genre) #-----FIX-----#
+    genre = models.ManyToManyField(Genre)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
 
     def __str__(self):
         return self.title
-    
-    def averageReview(self):
-        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
-        avg = 0
-        if reviews['average'] is not None:
-            avg = float(reviews['average'])
-        return avg
 
 
 class ReviewRating(models.Model):
     game = models.ForeignKey(Games, on_delete=models.CASCADE)
     review = models.TextField(max_length=500, blank=True)
     rating = models.FloatField()
-    status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     author = models.ForeignKey(
